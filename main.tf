51c9cce1919ee69b292e5a02cd1da428c6a5f63e
pagibso9812 <49698248+pagibso9812@users.noreply.github.com>
2019-09-09 12:56:13 -0600

    Update main.tf
    Changed AWS provider to just have version

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+#provider "aws" {
+#  access_key = ""
+#  secret_key = ""
+#  region     = ""
+#}
+
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
 }
+
 
 resource "aws_instance" "web" {
   # ...
